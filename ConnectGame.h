252c04e2d7233f498257c294f2fdf4cc4de57e53
Chris A. Long <long.chris.619@gmail.com>
2024-12-26 13:10:58 -0800

    Updated sequence finding function signatures in header file

--- ConnectGame.h
+++ ConnectGame.h
@@ -21,7 +21,7 @@ class ConnectGame {
 		int numToWin;
 		bool PlacePiece(std::string userInput, ConnectGame::Player player);
 		std::vector<std::vector<int>> gameBoard;
-		bool CheckDiagonal(ConnectGame::Player player);
-		bool CheckRow(ConnectGame::Player player);
-		bool CheckColumn(ConnectGame::Player player);
+		bool CheckDiagonal(ConnectGame::Player player, int row, int column);
+		bool CheckRow(ConnectGame::Player player, int row, int column);
+		bool CheckColumn(ConnectGame::Player player, int row, int column);
 };
